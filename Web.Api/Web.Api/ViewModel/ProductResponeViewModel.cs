4cc3f4763f8cbea5b987519faad16ec58ec00c9f
nguyentronger98 <65801851+nguyentronger98@users.noreply.github.com>
2022-02-16 14:31:18 +0700

    update

---
+++ Web.Api/Web.Api/ViewModel/ProductResponeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Api.ViewModel
+{
+    public class ProductResponeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public ICollection<string> ImagesProduct { get; set; }
+    }
+}
